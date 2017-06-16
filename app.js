e2f90ca25bd9060146f142b8f8aca47c67bbeb5b
Kristoffer Panahon <krp312@me.com>
2017-06-16 15:05:44 -0400

    finished toggle function

--- app.js
+++ app.js
@@ -4,15 +4,26 @@
 
 //1. Design State
 
-const initialState = {
-	//$(".shopping-item")
-}
+const initialState = [];
 
 //2. State Modifier Functions
 
 	//Add a shopping list item
 
+	// function addListItem(state, listItem) {
+	// 	state.some-key = listItem;
+	// }
+
 	//Toggleclass for .shopping-item__checked 
+
+	function checkToggle() {
+		$('button.shopping-item-toggle').click(
+			function(event) {
+				$(event.target).closest('li').children('span').toggleClass('shopping-item__checked');
+			}
+		);
+	}
+	checkToggle();
 
 	//Somehow delete the whole food div
 
