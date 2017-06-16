038e3fff99760f2df069c3cb695fd66c0f464009
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-06-16 13:59:32 -0400

    first commit, just explanation really

---
+++ app.js
@@ -0,0 +1,36 @@
+//add items with the add item button and return key
+//when check is clicked toggle the line-through class 
+//when delete is clicked remove the whole div
+
+//1. Design State
+
+const initialState = {
+	//$(".shopping-item")
+}
+
+//2. State Modifier Functions
+
+	//Add a shopping list item
+
+	//Toggleclass for .shopping-item__checked 
+
+	//Somehow delete the whole food div
+
+	//Find the value for the text input
+
+//3. Render Functions
+
+	//Function(s) that happen when you clicked
+
+//4. Event listener
+
+	//Add item click function (with button and return key)
+
+	//Check click function
+
+	//Delete function
+
+
+
+
+
