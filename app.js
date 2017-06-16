d1c73b5de562b1eaeeb7b08383fe25f415f98ce4
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-06-16 16:10:14 -0400

    added deleteItem functions

--- app.js
+++ app.js
@@ -4,16 +4,25 @@
 
 //1. Design State
 
-const initialState = [];
+let appState = {
+	index: 0,
+	items: []
+};
 
 //2. State Modifier Functions
 
 	//Add a shopping list item
 
-	// function addListItem(state, listItem) {
-	// 	state.some-key = listItem;
-	// }
+	function addListItem(state, listItem) {
+		state.items.push({
+			name: listItem, 
+			checked: false
+		});
 
+		// run new render 
+	}
+	addListItem(appState, "oranges");
+	console.log(appState);
 	//Toggleclass for .shopping-item__checked 
 
 	function checkToggle() {
@@ -26,6 +35,37 @@ const initialState = [];
 	checkToggle();
 
 	//Somehow delete the whole food div
+
+	function deleteItem(state, item) {
+		//const targetIndex = state.items.indexOf(item);
+		let currentItem = ;
+		state.items.splice(currentItem, 1);
+
+		//splice(targetIndex, 1);
+	}
+	function handleItemDeletes(listElem, itemElem){
+		// does the list elem have a length?
+		// can you use that to find the item at the same
+		// position in the state?
+	}
+
+	// separation of concerns
+
+	// const item1 = {
+	// 	name: 'apple', 
+	// 	checked: false
+	// }
+	// const item2 = {
+	// 	name: 'apple', 
+	// 	checked: false
+	// }
+
+	// // console.log(item1===item2)
+
+	// const testArr = [item2];
+	// console.log(testArr.indexOf(item1))
+
+
 
 	//Find the value for the text input
 
@@ -40,8 +80,3 @@ const initialState = [];
 	//Check click function
 
 	//Delete function
-
-
-
-
-
